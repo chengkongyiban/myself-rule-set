9a6f618da290dbf89531924d5e5d01461bfb483d
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-20 17:02:24 +0800

    更新 mock-rocket.js

--- jspart/mock-rocket.js
+++ jspart/mock-rocket.js
@@ -1,1 +1,1 @@
-body=body.replace(/(RULE-SET,Private,DIRECT)/,"$1\n  - DOMAIN,baidu.com,DIRECT\n  -  DOMAIN,qqq.com,DIRECT")
+body=body.replace(/(RULE-SET,Private,DIRECT)/,"$1\n  - DOMAIN,baidu.com,DIRECT\n  - DOMAIN,qqq.com,DIRECT")
